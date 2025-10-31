e9c9c0084ae2e208a20cbb158f1482d97b6463af
jaynemd <mbappepes254@gmail.com>
2025-11-01 02:23:16 +0300

    Update pair.js

--- pair.js
+++ pair.js
@@ -20,7 +20,7 @@ function removeFile(FilePath){
 router.get('/', async (req, res) => {
     const id = makeid();
     let num = req.query.number;
-        async function JAYNE_MD_PAIR_CODE() {
+        async function GIFTED_MD_PAIR_CODE() {
         const {
             state,
             saveCreds
